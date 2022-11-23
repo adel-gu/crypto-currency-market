0ae03f2cc3e80a42503bc9a78082ca7edb7a6fe0
adelguit <adeelguitoun@gmail.com>
2022-11-23 16:54:30 +0100

    Configure the store for the coins reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import categories from './categories/categories';
+import coins from './coins/coins';
 
 const rootReducer = {
   reducer: {
     cat: categories,
+    coins: coins,
   },
 };
 
