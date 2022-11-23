90f7aa6109fc0e0bde667592eec46e486b2b3b84
adelguit <adeelguitoun@gmail.com>
2022-11-23 16:55:09 +0100

    Dispatch the action for retrieving coins, and desplaying coins to the UI

--- src/components/CoinList.jsx
+++ src/components/CoinList.jsx
@@ -1,9 +1,22 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { retrieveCoins } from '../redux/coins/coins';
 import Coin from './Coin';
 
-const CoinList = () => (
-  <>
-    <Coin />
-  </>
-);
+const CoinList = () => {
+  const coins = useSelector(state => state.coins.coins);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(retrieveCoins());
+  }, []);
+
+  return (
+    <div className="row">
+      {
+        coins.map(coin => (<Coin key={coin.id} coin={coin}/>))
+      }
+    </div>
+  )
+};
 export default CoinList;
