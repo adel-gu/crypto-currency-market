ca8f90b652ba5badcdb39c86ab90c4f5f5773df7
adelguit <adeelguitoun@gmail.com>
2022-11-23 16:28:57 +0100

    Display coins categories in the drop-menu

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,17 +1,31 @@
+import { useEffect } from 'react';
 import { Container, Nav, NavDropdown } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
 import CoinList from '../components/CoinList';
+import { retrieveCat } from '../redux/categories/categories';
 
-const Home = () => (
-  <Container>
-    <Nav>
-      <NavDropdown title="Categroies" id="basic-nav-dropdown" className="border mx-auto">
-        <NavDropdown.Item>Ed</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-    <main>
-      <CoinList />
-    </main>
-  </Container>
-);
+const Home = () => {
+  const cats = useSelector(state => state.cat);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(retrieveCat());
+  })
+
+  return (
+    <Container>
+      <Nav>
+        <NavDropdown title="Categroies" id="basic-nav-dropdown" className="border mx-auto">
+          <NavDropdown.Item key={"all"}>All</NavDropdown.Item>
+          {
+            cats.map(cat => (<NavDropdown.Item key={cat["category_id"]}>{cat["name"]}</NavDropdown.Item>))
+          }
+        </NavDropdown>
+      </Nav>
+      <main>
+        <CoinList />
+      </main>
+    </Container>
+  );
+}
 export default Home;
