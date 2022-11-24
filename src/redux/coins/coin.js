810778a4834fff622b24e36f6eb3f07c30a434b9
adelguit <adeelguitoun@gmail.com>
2022-11-24 07:47:31 +0100

    Implement redux for single coin details data fetching

---
+++ src/redux/coins/coin.js
@@ -0,0 +1,40 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+// API
+const API_URL_COIN = 'https://api.coingecko.com/api/v3/coins';
+
+// Actions
+const GET_COIN = 'get-coin';
+
+// Action thunks
+export const retrieveCoinDetails = createAsyncThunk(GET_COIN, async (id) => {
+  const res = fetch(`${API_URL_COIN}/${id}`);
+  const data = (await res).json();
+  return data;
+});
+
+// Reducers
+const coinSlice = createSlice({
+  name: 'CryptoCoins/Coin',
+  initialState: {
+    coinDetails: {},
+    loading: false,
+  },
+  extraReducers: (builder) => {
+    builder.addCase(retrieveCoinDetails.pending, (state, action) => {
+      return {
+        ...state,
+        loading: true,
+      };
+    });
+    builder.addCase(retrieveCoinDetails.fulfilled, (state, action) => {
+      return {
+        ...state,
+        coinDetails: action.payload,
+        loading: false,
+      };
+    });
+  },
+});
+
+export default coinSlice.reducer;
