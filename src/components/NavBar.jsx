8e566f58f8d179552b4c4e2c143d2b8cdfbec9fb
adelguit <adeelguitoun@gmail.com>
2022-11-25 11:11:12 +0100

    Style the navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,36 @@
-import { Container, Navbar } from 'react-bootstrap';
+import { Container, Navbar, Button } from 'react-bootstrap';
+import { GiFrozenBlock } from 'react-icons/gi';
+import { BsGearFill, BsMicFill } from 'react-icons/bs';
+import { TiArrowLeftThick } from 'react-icons/ti';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import { StyledButton } from './StyledButton';
 
-const NavBar = () => (
-  <header>
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand className="mx-auto">Coins</Navbar.Brand>
-      </Container>
-    </Navbar>
-  </header>
+// Styled compoenent
+const StyledNav = styled(Navbar)`
+  background: #EC4C8B;
+`;
+
+const Logo = styled(GiFrozenBlock)`
+  color: white;
+`;
+
+const NavBar = () => {
+  const navigate = useNavigate();
+
+  return (
+    <header>
+      <StyledNav>
+        <Container>
+          <StyledButton icon={<TiArrowLeftThick />} onClick={() => navigate(-1)}/>
+          <Navbar.Brand className="mx-auto"><Logo className='display-6 fw-bold'/></Navbar.Brand>
+          <div className="d-flex justify-content-between align-items-center">
+            <StyledButton icon={<BsMicFill />} />
+            <StyledButton icon={<BsGearFill />} />
+          </div>
+        </Container>
+      </StyledNav>
+    </header>
 );
-
+}
 export default NavBar;
