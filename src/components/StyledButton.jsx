4a1c87fd11b8dbfb7a9b844b7e386eabfa0a7291
adelguit <adeelguitoun@gmail.com>
2022-11-25 11:10:30 +0100

    Add a styled button component

---
+++ src/components/StyledButton.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Button } from 'react-bootstrap';
+
+
+const Btn = styled(Button)`
+  background: none;
+  border: none;
+  color: white;
+
+  &:hover {
+    background: #e6d82f;
+    color: #EC4C8B;
+  };
+`;
+
+export const StyledButton = (props) => {
+  return (
+    <Btn type="button" className="d-flex justify-content-center align-items-center rounded-circle   p-2" >{props.icon}</Btn>
+  )
+}
